57a5ad457df91e8329c4056f090763bf9f3f3bc8
juhakkim <sunsays12@naver.com>
2022-04-05 09:49:27 +0900

    added 'your world'

--- Hello world/src/Helloworld.java
+++ Hello world/src/Helloworld.java
@@ -3,7 +3,8 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-
+		System.out.println("Hello his world");
+		System.out.println("Hello your world");
 	}
 
 }
